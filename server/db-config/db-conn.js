06dbc8f0c3ffd65f98c9666fa696534f0e2aa867
mdsaddamhossain1 <mdsaddam.hossain@navanarealestate.com>
2025-06-01 10:35:09 +0600

    db-config and server vercel update

--- server/db-config/db-conn.js
+++ server/db-config/db-conn.js
@@ -6,11 +6,17 @@ const connA = async () => {
   if (isConnected) return mongoose;
 
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // reduce timeout
-      bufferCommands: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    // const db = await mongoose.connect(process.env.MONGO_URI, {
+    //   serverSelectionTimeoutMS: 5000, // reduce timeout
+    //   bufferCommands: false,
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      maxPoolSize: 10,          // Maximum number of connections
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      socketTimeoutMS: 45000    // Close sockets after 45s of inactivity
     });
 
     isConnected = db.connections[0].readyState;
