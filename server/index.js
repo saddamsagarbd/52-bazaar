beeda86b83a6a7c5ee680f33a5fa2d8ed97d42d1
mdsaddamhossain1 <mdsaddam.hossain@navanarealestate.com>
2025-06-04 11:27:04 +0600

    server entry point modifide

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const { handler } = require('./server');
+const { handler, app } = require('./server');
 
 module.exports = handler;
 
 // Local development
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
-  require('./server').app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
