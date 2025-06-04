ff8928f994a6f3536bc202efe08676397d60427b
mdsaddamhossain1 <mdsaddam.hossain@navanarealestate.com>
2025-06-04 15:12:59 +0600

    debug in product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -7,4 +7,6 @@ const productController = require('../controllers/productController');
 router.get('/products', productController.getProducts);
 router.post('/add-product', upload.single("product_image"), productController.addProduct);
 
+console.log("Product Routes Loaded:", router.stack.map(layer => layer.route?.path).filter(Boolean));
+
 module.exports = router;
