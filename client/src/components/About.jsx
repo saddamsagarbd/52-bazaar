465a25ec513fc8987bd8b05e73193df4f65995cf
saddamsagar <saddamsagar02@gmail.com>
2025-06-06 17:24:53 +0600

    content update

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,13 +7,15 @@ const About = () => {
             <h3 className='text-4xl font-bold text-black text-center'>About 52 Bazaar</h3>
         </div>
         <div className='flex justify-center w-full max-w-[1100px] py-6'>
-            <div className="flex w-full">
-            <p className="w-1/2 text-black text-justify p-4">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <div className="w-1/2 flex items-center justify-center p-4">
-                <img src={Team} alt="52-bazaar-team" />
-            </div>
+            <div className="flex flex-col-reverse md:flex-row w-full">
+                <div className="w-full md:w-1/2 text-black text-justify p-4">
+                    <p>
+                        52 Bazaar is your trusted online shop dedicated to bringing you the best products at unbeatable prices. We promise quality and value by offering a wide range of items that are often cheaper than what you’d find at retail stores. Enjoy the convenience of shopping from home with the added benefit of cash on delivery, making your purchase secure and hassle-free. At 52 Bazaar, we strive to provide a seamless shopping experience with fast, reliable delivery right to your doorstep — because getting the best shouldn’t be complicated.
+                    </p>
+                </div>
+                <div className="w-full md:w-1/2 flex items-center justify-center p-4">
+                    <img src={Team} alt="52-bazaar-team" className="max-w-full h-auto" />
+                </div>
             </div>
         </div>
     </div>;
