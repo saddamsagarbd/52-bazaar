724ba0b531cde728d52c9bced43b385edfe5f4f0
saddamsagar <saddamsagar02@gmail.com>
2025-06-26 23:35:03 +0600

    update

--- server/db-config/db-conn.js
+++ server/db-config/db-conn.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let isConnected;
 
-const connA = async () => {
+export const connA = async () => {
   if (isConnected || mongoose.connection.readyState >= 1) return mongoose;
 
   try {
     const db = await mongoose.connect(process.env.MONGO_URI, {
-      maxPoolSize: 10,          // Maximum number of connections
+      maxPoolSize: 10, // Maximum number of connections
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-      socketTimeoutMS: 45000    // Close sockets after 45s of inactivity
+      socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
     });
 
     isConnected = db.connections[0].readyState;
@@ -19,5 +19,3 @@ const connA = async () => {
     throw error;
   }
 };
-
-export default {connA}
